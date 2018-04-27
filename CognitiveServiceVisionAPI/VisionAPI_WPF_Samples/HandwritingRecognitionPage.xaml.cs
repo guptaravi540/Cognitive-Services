70178b3757d5a32174dde8b1131565a4cc359db8
Abhimanyu Kumar Vatsa <abhimanyu@knorish.com>
2018-04-27 10:23:39 +0530

    typo

--- CognitiveServiceVisionAPI/VisionAPI_WPF_Samples/HandwritingRecognitionPage.xaml.cs
+++ CognitiveServiceVisionAPI/VisionAPI_WPF_Samples/HandwritingRecognitionPage.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 using Microsoft.ProjectOxford.Vision;
 using Microsoft.ProjectOxford.Vision.Contract;
 using System.IO;
